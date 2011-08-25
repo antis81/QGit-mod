1c9c5cbebfe2e920c88f96d6910cc875f706acd7
Ilya Kornev <stolzium@gmail.com>
2011-08-25 16:12:00 +0400

    Add search filter origin/branch

--- src/ui/referencetreeview.cpp
+++ src/ui/referencetreeview.cpp
@@ -38,6 +38,7 @@ void ReferenceTreeView::showAllItems(QModelIndex modelIndex)
 
 void ReferenceTreeView::showSearchedItems(QString inputText)
 {
+    //! @badcode FIXME: DO RECURSIVE SOLUTION
     showAllItems();
     expandAll();
 
@@ -48,53 +49,100 @@ void ReferenceTreeView::showSearchedItems(QString inputText)
     bool f = false;
     bool ff = false;
 
-    if (!(inputText.simplified().isEmpty())) {
+    QString firstPart;
+    QString lastPart;
+
+    inputText = inputText.simplified();
+
+    if (inputText.contains("/") && (inputText.indexOf("/") != (inputText.length() -1 ))) {
+        firstPart = inputText.left(inputText.indexOf("/"));
+        lastPart = inputText.mid(inputText.indexOf("/") + 1);
+        qDebug() << firstPart;
+        qDebug() << lastPart;
         for (int i = 0; i < model()->rowCount(QModelIndex()); i++) {
+            f = false;
             headerIndex = model()->index(i, 0, QModelIndex());
-
-            //[0]
-            if (isRegExpConformed(inputText, headerIndex.data().toString())) {
-                setRowHidden(i, QModelIndex(), false);
-            } else {
-                f = false;
+            if (headerIndex.data().toString() == "Remotes") {
                 for (int j = 0; j < model()->rowCount(headerIndex); j++) {
+                    ff = false;
                     subIndex = model()->index(j, 0, headerIndex);
+                    if (subIndex.data().toString() == firstPart) {
 
-                    if (isRegExpConformed(inputText, subIndex.data().toString())) {
-                        setRowHidden(j, headerIndex, false);
-                        if (!f)
-                            f = true;
-                    } else {
-                        ff = false;
-
-                        if (headerIndex.data().toString() == "Remotes") {
-                            for (int k = 0; k < model()->rowCount(subIndex); k++) {
-                                remoteLeafIndex = model()->index(k, 0, subIndex);
-
-                                if (isRegExpConformed(inputText, remoteLeafIndex.data().toString())) {
-                                    setRowHidden(k, subIndex, false);
-                                    if (!ff) {
-                                        ff = true;
-                                    }
-                                } else {
-                                    setRowHidden(k, subIndex, true);
+                        for (int k = 0; k < model()->rowCount(subIndex); k++) {
+                            remoteLeafIndex = model()->index(k, 0, subIndex);
+                            if (isRegExpConformed(lastPart, remoteLeafIndex.data().toString())) {
+                                if (!ff) {
+                                    ff = true;
                                 }
+                                setRowHidden(k, subIndex, false);
+                            } else {
+                                setRowHidden(k, subIndex, true);
                             }
                         }
-
-                        if (ff) {
-                            setRowHidden(j, headerIndex, false);
-                            f = true;
-                        }
-                        else
-                            setRowHidden(j, headerIndex, true);
+                    }
+                    if (!ff) {
+                        setRowHidden(j, headerIndex, true);
+                    } else {
+                        setRowHidden(j, headerIndex, false);
+                        f = true;
                     }
                 }
-                if (f)
+            }
+            if (!f)
+                setRowHidden(i, QModelIndex(), true);
+            else
+                setRowHidden(i, QModelIndex(), false);
+        }
+    } else {
+        if (!(inputText.isEmpty())) {
+            if (inputText.contains("/") && (inputText.indexOf("/")) == (inputText.length() - 1)) {
+                inputText = inputText.left(inputText.indexOf("/"));
+            }
+            for (int i = 0; i < model()->rowCount(QModelIndex()); i++) {
+                headerIndex = model()->index(i, 0, QModelIndex());
+
+                //[0]
+                if (isRegExpConformed(inputText, headerIndex.data().toString())) {
                     setRowHidden(i, QModelIndex(), false);
-                else
-                    setRowHidden(i, QModelIndex(), true);
-            } // [0]
+                } else {
+                    f = false;
+                    for (int j = 0; j < model()->rowCount(headerIndex); j++) {
+                        subIndex = model()->index(j, 0, headerIndex);
+
+                        if (isRegExpConformed(inputText, subIndex.data().toString())) {
+                            setRowHidden(j, headerIndex, false);
+                            if (!f)
+                                f = true;
+                        } else {
+                            ff = false;
+                            if (headerIndex.data().toString() == "Remotes") {
+                                for (int k = 0; k < model()->rowCount(subIndex); k++) {
+                                    remoteLeafIndex = model()->index(k, 0, subIndex);
+
+                                    if (isRegExpConformed(inputText, remoteLeafIndex.data().toString())) {
+                                        setRowHidden(k, subIndex, false);
+                                        if (!ff) {
+                                            ff = true;
+                                        }
+                                    } else {
+                                        setRowHidden(k, subIndex, true);
+                                    }
+                                }
+                            }
+                            if (ff) {
+                                setRowHidden(j, headerIndex, false);
+                                f = true;
+                            }
+                            else
+                                setRowHidden(j, headerIndex, true);
+                        }
+                    }
+                    if (f)
+                        setRowHidden(i, QModelIndex(), false);
+                    else
+                        setRowHidden(i, QModelIndex(), true);
+                }
+            }
         }
     }
 }
